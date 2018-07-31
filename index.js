b56e5ddfc65ceeb5df09ccb4e30a25bb1d518ee9
Sam Zhao <zhao.shengjun@gmail.com>
2018-07-31 22:25:29 +1000

    pass data in promise

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let delay = seconds =>
   new Promise((resolve, reject) => {
-    setTimeout(resolve, seconds * 1000);
+    setTimeout(() => {
+      resolve("The long wait is over.");
+    }, seconds * 1000);
   });
 
 console.log("Starting delay");
 
-delay(2).then(() => {
-  console.log("2 seconds");
+delay(2).then(message => {
+  console.log(message);
 });
