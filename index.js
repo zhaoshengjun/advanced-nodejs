bf937d34d94f67f0a3db43188b0d475ba20e8961
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-01 23:09:39 +1000

    use promisify with fs

--- index.js
+++ index.js
@@ -1,20 +1,7 @@
 var { promisify } = require("util");
+var fs = require("fs");
 
-let delay = (seconds, callback) =>
-  new Promise((resolve, reject) => {
-    if (seconds > 3) {
-      callback(new Error("it takes too long!"));
-    } else {
-      setTimeout(() => {
-        callback(null, "The long wait is over.");
-      }, seconds * 1000);
-    }
-  });
-
-console.log("Starting delay");
-
-var promiseDelay = promisify(delay);
-
-promiseDelay(5)
-  .then(console.log)
+var writeFile = promisify(fs.writeFile);
+writeFile("sample.txt", "This is a sample")
+  .then(() => console.log("file successfully created."))
   .catch(err => console.log(err.message));
