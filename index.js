d5c29be6dc674b5f87b89e72748cfcbb38d5b8cd
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-01 22:35:02 +1000

    handle error

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let delay = seconds =>
   new Promise((resolve, reject) => {
+    throw new Error("Oops!");
     setTimeout(() => {
       resolve("The long wait is over.");
     }, seconds * 1000);
@@ -10,4 +11,7 @@ console.log("Starting delay");
 delay(2)
   .then(console.log)
   .then(_ => 42)
-  .then(number => console.log(`Hello world, ${number}`));
+  .then(number => console.log(`Hello world, ${number}`))
+  .catch(err => {
+    console.log(err.message);
+  });
