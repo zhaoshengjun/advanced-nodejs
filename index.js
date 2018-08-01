cf28fe442935894cc1eb6dd43d215db778ffda12
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-01 23:19:51 +1000

    callback hell

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-var { promisify } = require("util");
 var fs = require("fs");
 
-var writeFile = promisify(fs.writeFile);
-writeFile("sample.txt", "This is a sample")
-  .then(() => console.log("file successfully created."))
-  .catch(err => console.log(err.message));
+var beep = () => process.stdout.write("\x07");
+
+const doStuffSequentially = () => {
+  console.log("starting");
+  setTimeout(() => {
+    console.log("waiting");
+    setTimeout(() => {
+      console.log("waiting some more");
+      fs.writeFile("file.txt", "sample file...", error => {
+        if (error) {
+          console.error(error);
+        } else {
+          beep();
+          console.log("file created");
+          setTimeout(() => {
+            beep();
+            fs.unlink("file.txt", error => {
+              if (error) {
+                console.error(error);
+              } else {
+                console.log("file removed");
+                console.log("sequential execute done.");
+              }
+            });
+          }, 3000);
+        }
+      });
+    }, 2000);
+  }, 1000);
+};
+
+doStuffSequentially();
