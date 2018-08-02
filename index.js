448e21078b751678013cbc532ca6eddc4a4dfbca
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-02 23:00:28 +1000

    mix promise and async

--- index.js
+++ index.js
@@ -1,34 +1,11 @@
 var fs = require("fs");
 var { promisify } = require("util");
-var writeFile = promisify(fs.writeFile);
-var unlink = promisify(fs.unlink);
 
-var beep = () => process.stdout.write("\x07");
-var delay = seconds =>
-  new Promise((res, rej) => {
-    setTimeout(res, seconds * 1000);
-  });
+var readdir = promisify(fs.readdir);
 
-const doStuffSequentially = async () => {
-  console.log("starting");
-  await delay(1);
-  console.log("waiting");
-  await delay(2);
-  try {
-    await writeFile("file.txt", "Sample file...");
-  } catch (err) {
-    console.error(err.message);
-  }
-  beep();
-  console.log("file created");
-  await delay(3);
-  try {
-    await unlink("file.txt");
-  } catch (err) {
-    console.error(err.message);
-  }
-  beep();
-  console.log("file removed");
-};
+async function start() {
+  var files = await readdir(__dirname);
+  console.log(files);
+}
 
-doStuffSequentially();
+start();
