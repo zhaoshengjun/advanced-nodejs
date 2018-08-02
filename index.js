49957ba986c1a6fa32e893939c7524941cd97dba
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-02 23:09:35 +1000

    promise race

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ let unlink = promisify(fs.unlink);
 let delay = seconds =>
   new Promise((resolve, reject) => setTimeout(resolve, seconds * 1000));
 
-Promise.all([delay(2), delay(3), delay(5), delay(4)]).then(() =>
+Promise.race([delay(2), delay(3), delay(5), delay(4)]).then(() =>
   console.log("done")
 );
