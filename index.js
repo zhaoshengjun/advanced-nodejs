1bfda43263bbfae00418c38de54c61e0a917389f
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-03 23:31:48 +1000

    graph tasks queue

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let logUpdate = require("log-update");
+let toX = () => "X";
 let delay = seconds =>
   new Promise(resolve => setTimeout(resolve, seconds * 1000));
 
@@ -6,10 +8,18 @@ let tasks = [delay(4), delay(6), delay(3), delay(13), delay(5)];
 class PromiseQueue {
   constructor(promises = [], concurrentCount = 1) {
     this.concurrentCount = concurrentCount;
-    this.total = promises.length();
+    this.total = promises.length;
     this.todo = promises;
     this.running = [];
     this.complete = [];
+  }
+  graphTasks() {
+    let { todo, running, complete } = this;
+    logUpdate(`
+    todo:[${todo.map(toX)}],
+    running:[${running.map(toX)}],
+    complete:[${complete.map(toX)}],
+    `);
   }
 
   get runAnother() {
@@ -21,12 +31,15 @@ class PromiseQueue {
       let promise = this.todo.shift();
       promise.then(() => {
         this.complete.push(this.running.shift());
+        this.graphTasks();
         this.run();
       });
 
       this.running.push(promise);
+      this.graphTasks();
     }
   }
 }
 
 var delayQueue = new PromiseQueue(tasks, 2);
+delayQueue.run();
