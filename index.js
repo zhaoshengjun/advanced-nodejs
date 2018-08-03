d6b2e4e35be04de14e209134046c6b1226704040
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-03 23:08:37 +1000

    setup for courrent task queue

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
-let fs = require("fs");
-let { promisify } = require("util");
-
-let readdir = promisify(fs.readdir);
-let writeFile = promisify(fs.writeFile);
-let unlink = promisify(fs.unlink);
-
 let delay = seconds =>
-  new Promise((resolve, reject) => setTimeout(resolve, seconds * 1000));
+  new Promise(resolve => setTimeout(resolve, seconds * 1000));
 
-Promise.race([delay(2), delay(3), delay(5), delay(4)]).then(() =>
-  console.log("done")
-);
+let tasks = [delay(4), delay(6), delay(3), delay(13), delay(5)];
+
+class PromiseQueue {
+  constructor(promises = [], concurrentCount = 1) {
+    this.concurrentCount = concurrentCount;
+    this.total = promises.length();
+    this.todo = promises;
+    this.running = [];
+    this.complete = [];
+  }
+
+  get runAnother() {
+    return this.running.length < this.concurrentCount && this.todo.length;
+  }
+
+  run() {
+    while (this.runAnother) {
+      let promise = this.todo.shift();
+      promise.then(() => {
+        this.complete.push(this.running.shift());
+        this.run();
+      });
+
+      this.running.push(promise);
+    }
+  }
+}
+
+var delayQueue = new PromiseQueue(tasks, 2);
