d39ac4e84cfc8d9314e7183e8fb61e2d40fe7cdd
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-05 21:56:10 +1000

    ask for chunk in non-flowing  mode

--- stream.js
+++ stream.js
@@ -1,4 +1,18 @@
-process.stdin.on("data", chunk => {
-  let text = chunk.toString().trim();
-  console.log("echo: ", text);
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./test.mp3");
+
+readStream.on("data", chunk => {
+  console.log("reading little chunk\n", chunk.length);
 });
+
+readStream.on("end", () => console.log("read stream finished"));
+
+readStream.on("error", error => {
+  console.log("an error has occured");
+  console.error(error.message);
+});
+
+readStream.pause();
+
+process.stdin.on("data", chunk => readStream.read());
