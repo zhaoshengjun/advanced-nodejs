971b172f712ead2a42950754b26eafea5d653ebf
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-05 21:44:22 +1000

    use encoding option to read stream as object

--- stream.js
+++ stream.js
@@ -11,7 +11,7 @@ const peaks = [
 
 class StreamFromArray extends Readable {
   constructor(array) {
-    super();
+    super({ encoding: "UTF-8" });
     this.array = array;
     this.index = 0;
   }
