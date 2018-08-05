f2cdee31221aafbd6afb7dd7496d36b2084c246f
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-05 21:46:41 +1000

    object mode

--- stream.js
+++ stream.js
@@ -11,14 +11,17 @@ const peaks = [
 
 class StreamFromArray extends Readable {
   constructor(array) {
-    super({ encoding: "UTF-8" });
+    super({ objectMode: true });
     this.array = array;
     this.index = 0;
   }
 
   _read() {
     if (this.index <= this.array.length) {
-      const chunk = this.array[this.index];
+      const chunk = {
+        data: this.array[this.index],
+        index: this.index
+      };
       this.push(chunk);
       this.index += 1;
     } else {
