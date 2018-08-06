27aaee75d3a0a0fe24f4539dc19e4b61796a081f
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-06 22:20:46 +1000

    backpressure and drain

--- stream.js
+++ stream.js
@@ -4,7 +4,12 @@ const readStream = createReadStream("./test.mp3");
 const writeStream = createWriteStream("./copy.mp3");
 
 readStream.on("data", chunk => {
-  writeStream.write(chunk);
+  const result = writeStream.write(chunk);
+  if (!result) {
+    // hose is full
+    console.log("backpressure");
+    readStream.pause();
+  }
 });
 
 readStream.on("end", () => writeStream.end());
@@ -12,6 +17,10 @@ readStream.on("end", () => writeStream.end());
 readStream.on("error", error => {
   console.log("an error has occured");
   console.error(error.message);
+});
+writeStream.on("drain", () => {
+  console.log("drained");
+  readStream.resume();
 });
 
 writeStream.on("close", () => process.stdout.write("file copied!"));
