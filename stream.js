ab0c29b83a2a26cafcb19f5df826f6118d4c1f91
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-06 22:36:33 +1000

    stdin pipe

--- stream.js
+++ stream.js
@@ -1,5 +1,4 @@
-const { createReadStream, createWriteStream } = require("fs");
+const { createWriteStream } = require("fs");
 
-const readStream = createReadStream("./test.mp3");
-const writeStream = createWriteStream("./copy.mp3");
-readStream.pipe(writeStream).on("error", console.error);
+const writeStream = createWriteStream("./test.txt");
+process.stdin.pipe(writeStream).on("error", console.error);
