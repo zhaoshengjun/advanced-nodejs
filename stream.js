0fbd73105c117ee98da8d47c4c3df3aabb89da8d
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-06 22:34:56 +1000

    use pipe to simplify the stream

--- stream.js
+++ stream.js
@@ -1,28 +1,5 @@
 const { createReadStream, createWriteStream } = require("fs");
 
 const readStream = createReadStream("./test.mp3");
-const writeStream = createWriteStream("./copy.mp3", {
-  highWaterMark: 1628920
-});
-
-readStream.on("data", chunk => {
-  const result = writeStream.write(chunk);
-  if (!result) {
-    // hose is full
-    console.log("backpressure");
-    readStream.pause();
-  }
-});
-
-readStream.on("end", () => writeStream.end());
-
-readStream.on("error", error => {
-  console.log("an error has occured");
-  console.error(error.message);
-});
-writeStream.on("drain", () => {
-  console.log("drained");
-  readStream.resume();
-});
-
-writeStream.on("close", () => process.stdout.write("file copied!"));
+const writeStream = createWriteStream("./copy.mp3");
+readStream.pipe(writeStream).on("error", console.error);
