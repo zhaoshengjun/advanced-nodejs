6af92308fce48dfe7a0c05b4f244feac205e5f8e
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-08 22:40:31 +1000

    create a media streaming server

---
+++ server.js
@@ -0,0 +1,9 @@
+const { createServer } = require("http");
+const { createReadStream } = require("fs");
+
+const file = "./test.mp3";
+
+createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "music/mp3" });
+  createReadStream(file).pipe(res);
+}).listen(3000, () => console.log("Server is running on port 3000"));
