31f810b78924c4c57abeecd4d27f25222ec8f15e
Sam Zhao <zhao.shengjun@gmail.com>
2018-08-08 22:46:34 +1000

    output file size

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const { createServer } = require("http");
-const { createReadStream } = require("fs");
-
+const { stat, createReadStream } = require("fs");
+const { promisify } = require("util");
 const file = "./test.mp3";
+const fileInfo = promisify(stat);
 
-createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "audio/mp3" });
+createServer(async (req, res) => {
+  const { size } = await fileInfo(file);
+  res.writeHead(200, {
+    "Content-Type": "audio/mp3",
+    "Content-Length": size
+  });
   createReadStream(file).pipe(res);
 }).listen(3000, () => console.log("Server is running on port 3000"));
